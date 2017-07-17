b75a30c836f03906484b7fe9d1cdf9150e99c297
hawkroc <yinpengroc@gmail.com>
2017-07-18 11:26:48 +1200

    'changeremote'

--- server.js
+++ server.js
@@ -1,43 +1,54 @@
 //server.js
 'use strict'
-const  schedule = require('./server/schedule');
+const schedule = require('./server/schedule');
 const express = require('express');
 
 const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
-
+const mogo = require('./server/mongooseProxy');
 const port = 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
-app.use((req, res, next) =>{
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
 });
 //test api
-router.get('/', (req, res)=> {
-  res.json({ message: 'API success?'});
+router.get('/', (req, res) => {
+    res.json({ message: 'API success?' });
 });
 
 
+router.route('/groupby/:keyword')
+
+.get((req, res) => {
+    GetDataGroupByDate();
+});
 
 
+router.route('/get/:address')
 
- scheduleCronstyle();
+.get((req, res) => {
+    GetDataGroupByDate();
+});
+
+
+scheduleCronstyle();
 
 app.use('/api', router);
 
 
 
 
-app.listen(port, ()=> {
-  console.log(`api running on port ${port}`);
+app.listen(port, () => {
+    console.log(`api running on port ${port}`);
 });
---
+++ server/mongooseProxy.js
@@ -0,0 +1,124 @@
+const Record = require('../model/records');
+
+// var Potato = mongoose.model('Potato', PotatoSchema);
+
+// var potatoBag = [/* a humongous amount of potato objects */];
+
+// Potato.collection.insert(potatoBag, onInsert);
+
+// function onInsert(err, docs) {
+//     if (err) {
+//         // TODO: handle error
+//     } else {
+//         console.info('%d potatoes were successfully stored.', docs.length);
+//     }
+// }
+
+CreateRecords = (array, address) => {
+
+    array.map((item) => {
+        item.time = new Date(item.timeStamp * 1000).toLocaleDateString();
+        item.address = address;
+        item.blockNumber = parseInt(item.blockNumber);
+    });
+
+
+    return Record.insertMany(array, (err, docs) => {
+        if (err) {
+            // TODO: handle error
+        } else {
+            console.info('%d potatoes were successfully stored.', docs.length);
+        }
+
+
+    });
+
+}
+
+
+
+GetMaxBlock = () => {
+
+    return Record.findOne()
+        // .select('record.blockNumber')
+        .sort({ "blockNumber": -1 }) // give me the max
+        .exec((err, record) => {
+            return record;
+
+
+        });
+}
+
+GetDataGroupByDate = () => {
+
+    return Record.aggregate([{
+            $project: {
+                address:"$address",
+                value:"$value",
+                gas:"$gas",
+                gasPrice:"$gasPrice",
+                year: { $year: "$time" },
+                month: { $month: "$time" },
+                day: { $dayOfMonth: "$time" },
+
+            }
+        }, 
+        {
+            $group: {
+             _id : {year:"$year",month:"$month"},
+           totalPrice: { $sum: { $multiply: [ "$gas", "$gasPrice" ] }},
+           
+            }
+        }
+
+    ], (err, result) => {
+        if (err) {
+
+            console.log("this is error ");
+        } else {
+              let rs=JSON.stringify(result);
+                console.log("this is successfully "+ JSON.stringify(result));
+              //  JSON.stringify(result);
+        }
+    });
+}
+
+
+
+GetDataRecords = () => {
+
+    return Record.aggregate([{
+            $project: {
+                address:"$address",
+                value:"$value",
+                gas:"$gas",
+                gasPrice:"$gasPrice",
+                year: { $year: "$time" },
+                month: { $month: "$time" },
+                day: { $dayOfMonth: "$time" },
+
+            }
+        }, 
+        {
+            $group: {
+             _id : {year:"$year",month:"$month"},
+           totalPrice: { $sum: { $multiply: [ "$gas", "$gasPrice" ] }},
+           
+            }
+        }
+
+    ], (err, result) => {
+        if (err) {
+
+            console.log("this is error ");
+        } else {
+              let rs=JSON.stringify(result);
+                console.log("this is successfully "+ JSON.stringify(result));
+              //  JSON.stringify(result);
+        }
+    });
+
+
+
+}
+
