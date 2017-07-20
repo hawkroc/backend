d2c29906fc72748340b0c732cc98b7135990d669
hawkroc <yinpengroc@gmail.com>
2017-07-20 15:45:51 +1200

    'addsetting'

---
+++ src/component/setting.js
@@ -0,0 +1,150 @@
+import { Table, Input, Icon, Button, Popconfirm } from 'antd';
+import React, { Component } from 'react';
+class EditableCell extends React.Component {
+  state = {
+    value: this.props.value,
+    editable: false,
+  }
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({ value });
+  }
+  check = () => {
+    this.setState({ editable: false });
+    if (this.props.onChange) {
+      this.props.onChange(this.state.value);
+    }
+  }
+  edit = () => {
+    this.setState({ editable: true });
+  }
+  render() {
+    const { value, editable } = this.state;
+    return (
+      <div className="editable-cell">
+        {
+          editable ?
+            <div className="editable-cell-input-wrapper">
+              <Input
+                value={value}
+                onChange={this.handleChange}
+                onPressEnter={this.check}
+              />
+              <Icon
+                type="check"
+                className="editable-cell-icon-check"
+                onClick={this.check}
+              />
+            </div>
+            :
+            <div className="editable-cell-text-wrapper">
+              {value || ' '}
+              <Icon
+                type="edit"
+                className="editable-cell-icon"
+                onClick={this.edit}
+              />
+            </div>
+        }
+      </div>
+    );
+  }
+}
+
+class Setting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.columns = [{
+      title: 'name',
+      dataIndex: 'name',
+      width: '30%',
+      render: (text, record, index) => (
+        <EditableCell
+          value={text}
+          onChange={this.onCellChange(index, 'name')}
+        />
+      ),
+    }, 
+    {
+      title: 'address',
+      dataIndex: 'address',
+       width: '30%',
+      render: (text, record, index) => (
+        <EditableCell
+          value={text}
+          onChange={this.onCellChange(index, 'address')}
+        />
+      ),
+      
+    }, {
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (text, record, index) => {
+        return (
+          this.state.dataSource.length > 0 ?
+          (
+            <Popconfirm title="Sure to delete?" onConfirm={() => this.onDelete(index)}>
+              <a href="#">Delete</a>
+            </Popconfirm>
+          ) : null
+        );
+      },
+    }];
+
+    this.state = {
+      dataSource: [{
+        key: '0',
+        name: 'Centrality Business Account',
+        age: '32',
+        address: '0x5ed8cee6b63b1c6afce3ad7c92f4fd7e1b8fad9f',
+      }, {
+        key: '1',
+        name: 'Blockhaus Swiss Account',
+        age: '32',
+        address: '0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae',
+      }],
+      count: 2,
+    };
+  }
+  onCellChange = (index, key) => {
+    return (value) => {
+      const dataSource = [...this.state.dataSource];
+      dataSource[index][key] = value;
+      this.setState({ dataSource });
+    };
+  }
+  onDelete = (index) => {
+    const dataSource = [...this.state.dataSource];
+    dataSource.splice(index, 1);
+    this.setState({ dataSource });
+  }
+  handleAdd = () => {
+    const { count, dataSource } = this.state;
+    const newData = {
+      key: count,
+      name: `input the Alias${count}`,
+      address: `input your Address. ${count}`,
+    };
+    this.setState({
+      dataSource: [...dataSource, newData],
+      count: count + 1,
+    });
+  }
+  render() {
+    const { dataSource } = this.state;
+    const columns = this.columns;
+    return (
+      <div>
+        <Button className="editable-add-btn" onClick={this.handleAdd}>Add</Button>
+        <Table bordered dataSource={dataSource} columns={columns} />
+      </div>
+    );
+  }
+}
+
+
+
+export default Setting;
+
+
+
