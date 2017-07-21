58714fcc60d4ad301df2c8914ba5723f16c5c315
hawkroc <yinpengroc@gmail.com>
2017-07-21 15:16:27 +1200

    'addexchange'

---
+++ src/component/exchange.js
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react';
+
+class Exchange extends Component {
+    static propTypes = {
+        className: PropTypes.string,
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            
+        <div>    
+<p className="marquee"><span>Bitcoin<a href="http://windows.microsoft.com/en-US/windows-8/start-screen">ETC</a>, <a href="http://windows.microsoft.com/en-US/windows-8/charms">ETH</a>, and a <a href="http://windows.microsoft.com/en-US/windows-8/microsoft-account">coin</a></span></p>
+
+<p className="vanity">
+ 
+</p>
+
+</div>
+
+        );
+    }
+}
+
+export default Exchange;
