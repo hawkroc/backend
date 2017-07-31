29ec77d7cbd463d3d4b57efe1e61e78ccf4cd83b
hawkroc <yinpengroc@gmail.com>
2017-07-31 16:59:42 +1200

    'addconfig'

---
+++ config_pro.js
@@ -0,0 +1,15 @@
+//dev
+
+var config = {
+ 
+  debug: false,
+  dbConfig:'mongodb://mongodb/blockeeper',
+  schedule:'1 * * * * *',
+  hardcode_address:'0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae',
+  key:'&apikey=E9MYVKUN5TNUBH6P4E5IWEUHAXGZCXQSNV',
+  baseUrl_dev:'/api/',
+  timeDiff:16000
+};
+module.exports = config;
+
+
