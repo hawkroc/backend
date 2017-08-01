04bc8d7fb6d959ee576f7664336c25cc9f78c5f1
hawkroc <yinpengroc@gmail.com>
2017-08-01 15:08:50 +1200

    'deploy'

--- config_prod.js
+++ config_prod.js
@@ -7,7 +7,7 @@ var config = {
   schedule:'1 * * * * *',
   hardcode_address:'0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae',
   key:'&apikey=E9MYVKUN5TNUBH6P4E5IWEUHAXGZCXQSNV',
-  baseUrl_dev:'/api/',
+  baseUrl_dev:'http://blockeeper-api.centrality.ai/api/',
   timeDiff:16000
 };
 module.exports = config;
