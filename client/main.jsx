c59ee2aa1d67d19f19b1abd8367581804269ad85
Tim Ruddell <tim@centrality.ai>
2017-08-09 09:58:43 +1200

    (BK-11) Adds initial redux project structure and wireup

--- client/main.jsx
+++ client/main.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import '../imports/startup/accounts-config.js';
-import App from '../imports/ui/App.jsx';
-import './style.css';
+import { Meteor } from 'meteor/meteor'
 
+import React from 'react'
+import { render } from 'react-dom'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from  './imports/redux/reducers'
+
+import App from '../imports/ui/App.jsx'
+import './style.css'
+
+const store = createStore(rootReducer)
  
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>, 
+    document.getElementById('react-root')
+  );
 });
-
-
-
-// ReactDOM.render(
-//   <App/>,
-//   document.getElementById('root')
-// );
---
+++ imports/redux/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+//import cart from './cart';
+
+/**
+ * Root redux reducer provided to createStore.
+ * 
+ */
+
+const rootReducer = combineReducers({
+    
+});
+
+export default rootReducer;
