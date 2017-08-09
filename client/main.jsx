05796bf9a32a8410da56c5c266c69f44977dae18
hawkroc <yinpengroc@gmail.com>
2017-08-09 12:25:11 +1200

    'changetoEnglish'

--- client/main.jsx
+++ client/main.jsx
@@ -6,16 +6,20 @@ import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from  '../imports/redux/reducers'
-
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 import App from '../imports/ui/App.jsx'
 import './style.css'
+//const { LocaleProvider, locales } = window.antd;
 
 const store = createStore(rootReducer)
  
 Meteor.startup(() => {
   render(
     <Provider store={store}>
+      <LocaleProvider locale={enUS}>
       <App />
+      </LocaleProvider>
     </Provider>, 
     document.getElementById('react-root')
   );
