661906b4fec71347a81e53d872a15f702e368de9
Tim Ruddell <tim@centrality.ai>
2017-08-15 16:19:55 +1200

    (BK-13) Makes label type serverside testing up-to-date

--- server/testing/api/profiles.tests.js
+++ server/testing/api/profiles.tests.js
@@ -11,8 +11,8 @@ import trackedAccountMethodTypes from '../../../imports/api/profiles/methods/tra
 
 
 /**
- * TODO: once the user model is correctly wire up and implemented, we'll 
- * need a lot of tests designed around testing permissions over profiles.
+ * TODO: once the user model is correctly wired up and implemented, we'll 
+ * need a lot of tests designed around ownership over profiles.
  * 
  */
 
@@ -21,7 +21,7 @@ describe('Profiles collection API', function () {
         resetDatabase();
     })
 
-    it('Adds a new label type to current profile', function () {
+    it('Adds a new label type to active profile', function () {
 
         let profile = Factory.create('profile')
         let initialLabelTypeCount = profile.labelTypes.length;
@@ -45,13 +45,18 @@ describe('Profiles collection API', function () {
         chai.assert.equal(newLabelTypeDetailCount, 1, "Only one label type has our specified details")
     })
 
-    it('Removes an existing label type from the current profile', function () {
+    it('Removes an existing label type from the active profile', function () {
         let profile = Factory.create('profile')
         let initialLabelTypeCount = profile.labelTypes.length;
 
         chai.assert.isAbove(initialLabelTypeCount, 0, "Initial label types exist to delete")
 
-        let labelTypeToDeleteId = profile.labelTypes[0]._id
+        let targetIndex = faker.random.number({
+            'min': 0,
+            'max': initialLabelTypeCount -1
+        });
+
+        let labelTypeToDeleteId = profile.labelTypes[targetIndex]._id
 
         Meteor.call(
             labelMethodTypes.PROFILE_DELETE_LABELTYPE,
@@ -68,5 +73,84 @@ describe('Profiles collection API', function () {
 
         // Make sure the correct label was deleted.
         chai.assert.equal(newLabelTypeDetailCount, 0, "We deleted the correct label type")
+    })
+
+    it('Removes labels associated with deleted label type', function () {
+        let profile = Factory.create('profile')
+        let initialLabelTypeCount = profile.labelTypes.length;
+
+        chai.assert.isAbove(initialLabelTypeCount, 1, "There are at least 2 label types to delete and compare")
+
+        let targetIndex = faker.random.number({
+            'min': 0,
+            'max': initialLabelTypeCount -1
+        });
+
+        let targetLabelTypeId = profile.labelTypes[targetIndex]._id
+        let altLabelTypeId = profile.labelTypes.find(lt => lt._id != targetLabelTypeId)._id
+
+        const addLabel = (txId, labelTypeId) => {
+            Meteor.call(
+                labelMethodTypes.PROFILE_UPDATE_LABEL,
+                { txId, labelTypeId }
+            )
+        }
+
+        // Add some labels for the existing label types.
+        addLabel("0", targetLabelTypeId)
+        addLabel("1", altLabelTypeId)
+        addLabel("2", targetLabelTypeId)
+        addLabel("3", altLabelTypeId)
+
+        // Assume labels were added correctly? - Not what we're testing here.
+
+        // Remove the target label type.
+        Meteor.call(
+            labelMethodTypes.PROFILE_DELETE_LABELTYPE,
+            { _id: targetLabelTypeId }
+        )
+
+        chai.assert.equal(Profiles.active().labels.length, 2, "Exactly 2 matching labels were removed")
+
+        let targetLabelCount = Profiles.active().labels.filter(l => l._id === targetLabelTypeId).length
+        chai.assert.equal(targetLabelCount, 0, "Labels corresponding to deleted label type have been removed")
+    })
+
+    it('Updates an existing label type in the active profile', function () {
+        let profile = Factory.create('profile')
+        let initialLabelTypeCount = profile.labelTypes.length;
+
+        let targetIndex = faker.random.number({
+            'min': 0,
+            'max': initialLabelTypeCount -1
+        });
+
+        chai.assert.isAbove(initialLabelTypeCount, 1, "At least 2 initial label types exist to attempt to update")
+
+        let labelTypeToUpdateId = profile.labelTypes[targetIndex]._id
+        let labelTypeInitialGst = profile.labelTypes[targetIndex].gst
+
+        let updatedLabelTypeName = faker.lorem.sentence()
+
+        Meteor.call(
+            labelMethodTypes.PROFILE_UPDATE_LABELTYPE,
+            { _id: labelTypeToUpdateId, name: updatedLabelTypeName, gst: !labelTypeInitialGst }
+        )
+
+        let newLabelTypeCount = Profiles.active().labelTypes.length
+        chai.assert.equal(newLabelTypeCount, initialLabelTypeCount, "There is no change to the number of label types");
+
+        let updatedLabelType = Profiles.active().labelTypes
+            .find(lt => lt._id == labelTypeToUpdateId)
+
+        let miscLabelType = Profiles.active().labelTypes
+            .find(lt => lt._id != labelTypeToUpdateId)
+
+        chai.assert.isObject(updatedLabelType, "Our updated label type still exists")
+
+        // Make sure the correct label was updated.
+        chai.assert.equal(updatedLabelType.name, updatedLabelTypeName, "The target label type name was updated")
+        chai.assert.equal(updatedLabelType.gst, !labelTypeInitialGst, "The target label type GST value was updated")
+        chai.assert.notEqual(miscLabelType.name, updatedLabelTypeName, "Other label type name was not updated")
     })
 })
