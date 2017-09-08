f3ab53c596065bd28754ed690bd56e0bd74ba81f
Tim Ruddell <tim@centrality.ai>
2017-09-08 12:57:53 +1200

    (BK-25) Adds Azure application insights wireup

--- client/main.jsx
+++ client/main.jsx
@@ -10,24 +10,22 @@ import enUS from 'antd/lib/locale-provider/en_US'
 
 // Subscribe the client to all of our API collections.
 import subscribeCollections from '../imports/client/subscribeCollections'
-subscribeCollections.apply();
 
 import './style.css'
 
 import store from '../imports/redux/store'
-
-import HeaderContentLayout from '../imports/ui/layouts/headerContent'
-import BodyContentLayout from '../imports/ui/layouts/bodyContent'
 import RootContainer from '../imports/ui/containers/rootContainer'
 
  
 Meteor.startup(() => {
 
-    render(
-      <Provider store={store}>
-        <RootContainer/>
-      </Provider>,
+    subscribeCollections.apply();
 
-      document.getElementById('react-root')
+    render(
+        <Provider store={store}>
+            <RootContainer/>
+        </Provider>,
+
+        document.getElementById('react-root')
     );
 });
