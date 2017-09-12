12712fb066daa0b7830050b18df5b2ca8c885ffb
hawkroc <yinpengroc@gmail.com>
2017-09-12 17:04:37 +1200

    (BK-27)Consolidate mongo schemas

---
+++ imports/api/exchangeRates/schemas/ratesSchema.js
@@ -0,0 +1,32 @@
+import SimpleSchema  from 'simpl-schema'
+
+const Rate = new SimpleSchema({
+	_id: { type: String },
+
+	high: {
+		type: Number,
+		defaultValue: 0
+	},
+	time: {
+		type: Number,
+		defaultValue: 0
+	},
+	average: {
+		type: Number,
+		defaultValue: 0
+	},
+	open: {
+		type: Number,
+		defaultValue: 0
+	},
+	low: {
+		type: Number,
+		defaultValue: 0
+	},
+	volume: {
+		type: Number,
+		defaultValue: 0
+	}
+})
+
+export default Rate
