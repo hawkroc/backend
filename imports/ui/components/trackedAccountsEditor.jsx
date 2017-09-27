5cc54c55c4a80b11de55d4912cac8a21bc280372
peng <yinpengroc@gmail.com>
2017-09-27 14:03:11 +1300

    (BK-24)TolowerCaseAndNoticeUser

--- imports/ui/components/trackedAccountsEditor.jsx
+++ imports/ui/components/trackedAccountsEditor.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-import { Table, Button, Modal, Input } from 'antd'
+import { Table, Button, Modal, Input, message } from 'antd'
 import { buildColumns } from './trackedAccountsEditorColumns'
-
+import { Meteor } from "meteor/meteor";
 /**
  * Presents a grid containing accounts currently tracked by the application.
  * 
  *  TODO: editable cells.
  * 
  */
+
+
 class TrackedAccountsEditorComponent extends React.Component {
 	constructor(args) {
 		super(args)
@@ -16,9 +18,18 @@ class TrackedAccountsEditorComponent extends React.Component {
 		// Local state for minor changes.
 		this.state = {
 			addModalVisible: false,
+			validation: false,
 			addAlias: '',
 			addAddress: ''
 		}
+	}
+	error = (info) => {
+
+		message.config({
+			top: 303,
+			duration: 2,
+		  })
+		message.error(info)
 	}
 
 	showAddModal() {
@@ -38,16 +49,35 @@ class TrackedAccountsEditorComponent extends React.Component {
 	}
 
 	handleAddAddressChange = (e) => {
-		this.setState({ addAddress: e.target.value })
+
+
+		if (e.target.value) {
+			this.setState({ addAddress: e.target.value.toLowerCase().trim() })
+		}
+
 	}
 
-	submitAdd = () => {
+	submitAdd = (trackedAccounts) => {
+		if (!/^0x[a-zA-Z0-9]{40}$/.test(this.state.addAddress)) {
+			this.error('Please check your address')
+			return null
+		}
+		if (this.props.idToAddressBalance.filter(
+			ta => ta.address === this.state.addAddress
+		).length > 0) {
+			this.error('This address alread be tracked')
+			return null
+		}
+
 		this.props.onInsertTrackedAccount({
 			alias: this.state.addAlias,
 			address: this.state.addAddress
 		})
 
 		this.hideAddModal()
+
+
+
 	}
 
 	render() {
@@ -65,7 +95,7 @@ class TrackedAccountsEditorComponent extends React.Component {
 		} = this.props
 
 		// Build the column set for this table.
-		const columns = buildColumns({languageConfig, onUpdateTrackedAccount, onDeleteTrackedAccount, idToAddressBalance})
+		const columns = buildColumns({ languageConfig, onUpdateTrackedAccount, onDeleteTrackedAccount, idToAddressBalance })
 
 		return (
 			<div>
@@ -82,11 +112,10 @@ class TrackedAccountsEditorComponent extends React.Component {
 				<Modal
 					title={languageConfig.Track_new_account}
 					visible={this.state.addModalVisible}
-
-					onOk={() => this.submitAdd()}
+					onOk={() => this.submitAdd(trackedAccounts)}
 					onCancel={() => this.hideAddModal()}
 				>
-					<h3 style={{marginBottom: '8px'}}>{languageConfig.Account_alias}</h3>
+					<h3 style={{ marginBottom: '8px' }}>{languageConfig.Account_alias}</h3>
 					<Input size="large" placeholder="My new account"
 						value={this.state.addAlias} onChange={this.handleAddAliasChange} />
 					<br />
