c38879c9445c10c4ed7d4187e1508147be8c767f
peng <yinpengroc@gmail.com>
2017-10-06 15:54:03 +1300

    add init lables

--- server/fixtures/environments/development/profileFixtures.js
+++ server/fixtures/environments/development/profileFixtures.js
@@ -35,16 +35,190 @@ export default {
 							items: [
 								{
 									_id: new Meteor.Collection.ObjectID().toHexString(),
-									label: 'Staff salary'
+									label: 'Abschluss'
 								},
 								{
 									_id: new Meteor.Collection.ObjectID().toHexString(),
-									label: 'Travel expense'
+									label: 'Accruals'
 								},
 								{
 									_id: new Meteor.Collection.ObjectID().toHexString(),
-									label: 'Rent'
+									label: 'Accruals & reserves'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Accumulated profit'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Administration costs'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Advertising expenses'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Assets'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Balance'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Cash and Bank'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Cash ETH'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Costs'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Current Assets'
+								},
+
+
+
+
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Depreciation'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Depts'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Extraordinary income'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Extraordinary income/expenses'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Fiinancial costs'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Financial expenses'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Financial income'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Fixed Assets'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Income'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Income Statement'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Intercompany Expenses'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Liabilities'
+								},
+
+
+
+
+
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Marketing expenses'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Miscellaneous costs'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Non periode income/expenses'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Not paid up share capital'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Operating expenses'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Other current receivables'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Other liabilitie'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Other Operating expenses'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Participation'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Personnel costs'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'PROFIT/LOSS'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Receivables'
+								},
+
+
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Repairs & Maintenance'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Reserves'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Share capital'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Shareholder equity'
+								},
+
+								
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Short-term liabilities'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Tangible fixed Assets'
+								},
+								{
+									_id: new Meteor.Collection.ObjectID().toHexString(),
+									label: 'Taxes'
 								}
+
 							]
 						},
 
